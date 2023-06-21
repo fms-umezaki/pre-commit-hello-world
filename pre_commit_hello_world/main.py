296b683a7b2b0530471f3067dd031093009d597b
umezaki <umezaki@fullmarks.co.jp>
2023-06-21 15:51:55 +0900

    use sys.argv

--- pre_commit_hello_world/main.py
+++ pre_commit_hello_world/main.py
@@ -1,9 +1,5 @@
-import argparse
+import sys
 
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--file-name", nargs="+")
-
-    args = parser.parse_args()
-    print(args)
+    print(sys.argv)
